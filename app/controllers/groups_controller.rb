2fe474126d5d01efb61da3c8ac57dddefa705eb7
Yanan Sun <sunyanan99@hotmail.com>
2017-02-26 15:38:50 +0800

    check over commission when access edit/update/destory

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-before_action :authenticate_user! , only: [:new, :create]
+before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
 
   def index
     @groups = Group.all
@@ -11,6 +11,10 @@ end
 
 def edit
   @group = Group.find(params[:id])
+
+  if current_user != @group.user
+    redirect_to root_path, alert: "You have no permission."
+  end
 end
 
 def new
@@ -20,7 +24,7 @@ end
 def create
   @group = Group.new(group_params)
   @group.user = current_user
-  
+
   if @group.save
   redirect_to groups_path
   else
@@ -30,6 +34,9 @@ end
 
 def update
   @group = Group.find(params[:id])
+  if current_user != @group.user
+    redirect_to root_path, alter: "You have no permisson."
+  end
   if @group.update(group_params)
   redirect_to groups_path, notice: "Update Success!"
  else
@@ -39,6 +46,10 @@ end
 
 def destroy
   @group = Group.find(params[:id])
+
+  if current_user != @group.user
+    redirect_to root_path, alter: "You have no permisson."
+  end 
   @group.destroy
   flash[:alert] = "Group deleted."
   redirect_to groups_path
